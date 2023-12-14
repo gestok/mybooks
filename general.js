24bad865e659c45313ddd7e7e52076b6bc3963ad
George Chondromatidis <giorgos.cho@hotmail.com>
2023-12-14 18:46:15 +0200

    Update general.js

--- general.js
+++ general.js
@@ -1,11 +1,11 @@
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 };
 const app = firebase.initializeApp(firebaseConfig);
 const auth = app.auth();
